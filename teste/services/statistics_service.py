fbb5083503e3014ae63fcae31b1044dc0c943b99
Mrpsousa <marcos.sousa@lactec.org.br>
2021-02-17 00:15:33 -0300

    feature: terceiro cálculo, pronto

--- teste/services/statistics_service.py
+++ teste/services/statistics_service.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 #
 #TODO retorno do dados em forma de API
+#Pandoc - gerador de documentação
 #
 def requests_by_consumer():
     '''
@@ -38,11 +39,22 @@ def average_time():
        Comentário relativo ao código 
     '''
     consumer_table = pd.read_csv('out.csv')
-    proxy = consumer_table[['service_id', 'latency_proxy', 'latency_gateway', 'latency_request', 'count']].groupby('service_id').sum()
-    
-    i = 0
-    for data in range(len(proxy.index)):
-        print(f'{proxy.index[i]} : {proxy.iat[i,0] / proxy.iat[i,3]}')
-        i += 1
+    data_sum = consumer_table[['service_id', 'latency_proxy', 'latency_gateway', 'latency_request', 'count']].groupby('service_id').sum()
 
+    # print(data_sum)    
+
+    i = 0
+    array = []
+    for data in range(len(data_sum.index)):
+        value = { 'service_id': data_sum.index[i],
+        'average_latency_proxy': data_sum.iat[i,0] / data_sum.iat[i,3],
+        'average_latency_gateway': data_sum.iat[i,1] / data_sum.iat[i,3],
+        'average_latency_request':data_sum.iat[i,2] / data_sum.iat[i,3],
+        }
+        array.append(value)
+        # print(f'{data_sum.index[i]} : {data_sum.iat[i,0] / data_sum.iat[i,3]}')
+        # print(f'{data_sum.index[i]} : {data_sum.iat[i,1] / data_sum.iat[i,3]}')
+        # print(f'{data_sum.index[i]} : {data_sum.iat[i,2] / data_sum.iat[i,3]}')
+        i += 1
+    print(array)
     return status_msg.sucess_data_200("foi average_time")
--- teste/tests/test.py
+++ teste/tests/test.py
@@ -3,8 +3,9 @@ from rest_framework import status
 from django.urls import reverse
 from ..models import WorkData
 from ..utils.serializers import WorkDataSerializer
+import os
 
-#TODO tratar error 500
+
 class TesteTest(TestCase):
 
     client = Client()
@@ -15,31 +16,44 @@ class TesteTest(TestCase):
     def test_sucess_get_populate(self):
         response = self.client.get(reverse('populate'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_unsuccess_get_populate(self):
-        response = self.client.get(reverse('populate'))
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def test_sucess_get_generate_csv(self):
         response = self.client.get(reverse('generate_csv'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_unsuccess_get_generate_csv(self):
-        response = self.client.get(reverse('generate_csv'))
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
     def test_sucess_get_requests_by_consumer(self):
         response = self.client.get(reverse('requests_by_consumer'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_unsuccess_get_requests_by_consumer(self):
-        response = self.client.get(reverse('requests_by_consumer'))
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def test_sucess_get_requests_by_service(self):
         response = self.client.get(reverse('requests_by_service'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_sucess_get_average_time(self):
+        response = self.client.get(reverse('average_time'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    os.rename('../out.csv', '../old.csv')
+
+    def test_unsuccess_get_populate(self):
+        response = self.client.get(reverse('populate'))
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def test_unsuccess_get_generate_csv(self):
+        response = self.client.get(reverse('generate_csv'))
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def test_unsuccess_get_requests_by_consumer(self):
+        response = self.client.get(reverse('requests_by_consumer'))
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def test_unsuccess_get_requests_by_service(self):
         response = self.client.get(reverse('requests_by_service'))
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def test_unsuccess_get_average_time(self):
+        response = self.client.get(reverse('average_time'))
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    os.rename('old.csv', 'out.csv')
